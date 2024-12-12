ec080bde780928a4c1b48fcbd08b26088b99887f
chenyongfa <45999735+idoosmart@users.noreply.github.com>
2024-12-12 16:10:02 +0800

    修改path

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "IDOBlueProtocol",
-            path: "IDOBlueProtocol/IDOBlueProtocol",
+            path: "IDOBlueProtocol",
 //            resources: [.process("IDOBlueProtocol.bundle")],
             publicHeadersPath: "."
         )
